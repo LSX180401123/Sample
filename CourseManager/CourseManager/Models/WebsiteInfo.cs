98e3e8735f89d8bf5963fae4cac32335eef76906
LSX180401123 <3162695964@qq.com>
2019-12-11 17:46:44 +0800

    xiugaui

--- CourseManager/CourseManager/Models/WebsiteInfo.cs
+++ CourseManager/CourseManager/Models/WebsiteInfo.cs
@@ -7,12 +7,33 @@ namespace CourseManager.Models
 {
     public class WebsiteInfo
     {
-        public WebsiteInfo(string siteName, string copyRight)
+        public class ActionLink
         {
-            SiteName = siteName;
-            CopyRight = copyRight;
+            public string Name { get; set; }
+            public string Controller { get; set; }
+            public string Action { get; set; }
         }
-        public string SiteName { get; set; }
-        public string CopyRight { get; set; }
+        public const string SiteName = "课程管理系统";
+        private string p1;
+        private string p2;
+
+        public List<ActionLink> ActionLinks { get; set; }
+
+        public WebsiteInfo()
+        {
+            ActionLinks = new List<ActionLink>
+        {
+           new ActionLink{Name="主页",Controller="Home",Action="Index"},
+            new ActionLink{Name="关于",Controller="Home",Action="About"},
+             new ActionLink{Name="联系方式",Controller="Home",Action="Contact"},
+        };
+        }
+
+        public WebsiteInfo(string p1, string p2)
+        {
+            // TODO: Complete member initialization
+            this.p1 = p1;
+            this.p2 = p2;
+        }
     }
 }
